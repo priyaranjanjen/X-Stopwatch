077081dc1b954c621ba109a079da0e42ec0f49c2
priyaranjan <jena.priya13@gmail.com>
2024-05-09 14:05:36 +0530

    updated stopwatch file

--- src/stopwatch.jsx
+++ src/stopwatch.jsx
@@ -42,13 +42,13 @@ export default function Stopwatch() {
         let sec = Math.floor(elapsedtTime / (1000) % 60);
         let millsec = Math.floor((elapsedtTime % 1000) / 10);
 
-        min = String(min).padStart(2, "0");
+        // min = String(min).padStart(2, "0");
         sec = String(sec).padStart(2, "0");
         millsec = String(millsec).padStart(2, "0");
 
 
 
-        return `${min}:${sec}:${millsec}`;
+        return `${min}:${sec}`;
     }
 
     return (
