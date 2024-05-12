075a80462bda9eb013589dc07fb3477f48f4d1f4
priyaranjan <jena.priya13@gmail.com>
2024-05-12 08:37:46 +0530

    modifies stopwatch.jsx

--- src/stopwatch.jsx
+++ src/stopwatch.jsx
@@ -40,11 +40,11 @@ export default function Stopwatch() {
 
         let min = Math.floor(elapsedtTime / (1000 * 60) % 60);
         let sec = Math.floor(elapsedtTime / (1000) % 60);
-        let millsec = Math.floor((elapsedtTime % 1000) / 10);
+        // let millsec = Math.floor((elapsedtTime % 1000) / 10);
 
         // min = String(min).padStart(2, "0");
         sec = String(sec).padStart(2, "0");
-        millsec = String(millsec).padStart(2, "0");
+        // millsec = String(millsec).padStart(2, "0");
 
 
 
